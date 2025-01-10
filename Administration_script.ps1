a02d8128760dad589db3b054f6cfd54b64124a59
Felix Kibet <felixkibetrutto@gmail.com>
2025-01-10 10:39:01 +0300

    Business Central administration script

---
+++ Administration_script.ps1
@@ -0,0 +1,20 @@
+﻿# Check the current execution policy
+Write-Output "Current Execution Policy:"
+Get-ExecutionPolicy
+
+# Temporarily set execution policy to Bypass for this session
+Write-Output "Temporarily setting Execution Policy to Bypass for this session..."
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
+
+# Define the path to the navadmintool.ps1 script
+$NavAdminToolPath = "C:\Program Files\Microsoft Dynamics 365 Business Central\220\Service\navadmintool.ps1"
+
+Write-Output "Importing navadmintool.ps1..."
+Import-Module -Name $NavAdminToolPath
+
+# List all available cmdlets related to NAV
+Write-Output "Listing all available cmdlets related to NAV:"
+Get-Command *NAV*
+
+
+
