ee916dd78cc98809dc6c2abf0f82c70218246b42
Felix Kibet <felixkibetrutto@gmail.com>
2025-02-18 09:44:58 +0300

    Add Business Central license import, server instance management, and user listing
    - Added functionality to import Business Central license file.
    - Included command to list all available Business Central server instances.
    - Implemented server restart to apply configuration changes.
    - Added functionality to list all users in the BC220 instance along with their states.
    - Output installed Business Central apps and user details to GridView for easier inspection.
    
    Enhances server management and user monitoring capabilities in the script.

--- Administration_script.ps1
+++ Administration_script.ps1
@@ -16,5 +16,40 @@ Import-Module -Name $NavAdminToolPath
 Write-Output "Listing all available cmdlets related to NAV:"
 Get-Command *NAV*
 
+# Import a Business Central license file
+Import-NAVServerLicense -ServerInstance BC220 -LicenseFile "C:\path\to\your\license.flf"
+
+# Enable API endpoints (REST and ODATA)
+Set-NAVServerConfiguration BC220 -KeyName ODataServicesEnabled -KeyValue true #OData
+Set-NAVServerConfiguration BC220 -KeyName ApiServicesEnabled -KeyValue true #REST
+
+# Get BC running configurations
+Get-NAVServerConfiguration BC220
+
+# List all Business Central instances
+Get-NAVServerInstance
+
+# Restart the NAV server instance to apply configuration changes
+Restart-NAVServerInstance -ServerInstance BC220
+
+# View installed apps in Business Central (BC220) via grid view
+Get-NAVAppInfo -ServerInstance BC220 | Out-GridView
+
+# List all users in the BC220 server instance and their states
+Get-NAVServerUser -ServerInstance BC220 | Select-Object UserName,State | Out-GridView
+
+# Count the number of users in Business Central
+Get-NAVServerUser -ServerInstance BC220 | Measure-Object
 
 
+# Enable a specific user in Business Central (cereals\administrator)
+Set-NAVServerUser -ServerInstance BC220 -UserName "cereals\fkibet" -State Enabled
+
+# Check user permissions for a specific user
+Get-NAVServerUserPermissionSet -ServerInstance BC220 -UserName "cereals\administrator"
+
+# Add a new user
+New-NAVServerUser -ServerInstance BC220 -UserName "cereals\administrator" -WindowsAccount
+
+# Remove an old user from Business Central
+Remove-NAVServerUser -ServerInstance BC220 -UserName "cereals\olduser"
